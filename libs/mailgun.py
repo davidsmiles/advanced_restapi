e3a3315403bba2d82f8f69ff6b7a3a32b8218a65
David Smiles <davidugbero@gmail.com>
2019-10-19 01:12:35 +0100

    implement custom error @class MailGunException

--- libs/mailgun.py
+++ libs/mailgun.py
@@ -1,20 +1,36 @@
 import os
 from typing import List
 
-from requests import Request, post
+from requests import Response, post
+
+
+FAILED_LOAD_API_KEY = "Failed to load MailGun API key."
+FAILED_LOAD_DOMAIN = "Failed to load MailGun domain."
+ERROR_SENDING_EMAIL = "Error in sending confirmation email, user registration failed."
+
+
+class MailGunException(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
 
 
 class Mailgun:
 
-    MAILGUN_DOMAIN = os.environ.get("MAILGUN_DOMAIN")
-    MAILGUN_API_KEY = os.environ.get("MAILGUN_API_KEY")
+    MAILGUN_DOMAIN = os.environ.get("MAILGUN_DOMAIN", None)
+    MAILGUN_API_KEY = os.environ.get("MAILGUN_API_KEY", None)
 
     FROM_TITLE = "Stores REST API"
     FROM_EMAIL = "ugberodavid@gmail.com"
 
     @classmethod
-    def send_email(cls, email: List[str], subject: str, text: str) -> Request:
-        return post(
+    def send_email(cls, email: List[str], subject: str, text: str) -> Response:
+        if cls.MAILGUN_API_KEY is None:
+            raise MailGunException(FAILED_LOAD_API_KEY)
+
+        if cls.MAILGUN_DOMAIN is None:
+            raise MailGunException(FAILED_LOAD_DOMAIN)
+
+        response = post(
             f"https://api.mailgun.net/v3/{cls.MAILGUN_DOMAIN}/messages",
             auth=("api", cls.MAILGUN_API_KEY),
             data={
@@ -24,3 +40,8 @@ class Mailgun:
                 "text": text
             },
         )
+
+        if response.status_code != 200:
+            raise MailGunException(ERROR_SENDING_EMAIL)
+
+        return response
